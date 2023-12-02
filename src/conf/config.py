452bfc04cffa83cf751f50be7f622d5faf6f8507
VadimMarenko <130446385+VadimMarenko@users.noreply.github.com>
2023-12-02 19:07:00 +0200

    Update config.py
    change extra parameter from "allow" to "ignore"

--- src/conf/config.py
+++ src/conf/config.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
-        extra = "allow"
+        extra = "ignore"
 
 
 # class Settings(BaseSettings):
